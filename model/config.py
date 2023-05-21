5458c1cfe90fe4024926ce10e0e880333e0e0ac4
Tokiniaina Toavina <67753001+ttoavina@users.noreply.github.com>
2023-05-21 21:32:03 +0300

    :wrench: Add configuration files.

---
+++ model/config.py
@@ -0,0 +1,22 @@
+from dataclasses import dataclass
+from typing import Dict, List
+from keras.models import Sequential
+import pickle
+
+@dataclass
+class Config:
+    class_to_idx : Dict[str, int]
+    idx_to_class : Dict[int, str]
+    model : Sequential
+    features_key_order: List
+
+    def save(self):
+        with open("config.dat","wb") as f:
+            pickle.dump(self, f)
+
+    @staticmethod
+    def load():
+        with open("config.dat", "r") as f:
+            config = pickle.load(f)
+
+        return config
